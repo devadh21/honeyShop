bfe4f826af6006340aa64ebfbd08685a6e309e26
devadh21 <devadh21@gmail.com>
2024-10-05 09:19:28 +0000

    Update next.config.mjs add image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [ 'res.cloudinary.com','images.unsplash.com', 'via.placeholder.com' ],
+        //domains: [ 'res.cloudinary.com','images.unsplash.com', 'via.placeholder.com' ],
+        remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+    ],
+        ],
 
       },
       
